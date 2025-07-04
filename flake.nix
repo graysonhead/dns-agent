438950d5b6daec19448b4004526e6a095656ab64
Grayson Head <grayson@graysonhead.net>
2025-07-04 10:37:22 -0500

    ci: Add Shell Hooks

--- flake.nix
+++ flake.nix
@@ -4,54 +4,76 @@
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
+  outputs = { self, flake-utils, naersk, nixpkgs, pre-commit-hooks }:
 
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
-
-        naersk' = pkgs.callPackage naersk { };
-
-        buildInputs = with pkgs; [
-          openssl.dev
-        ];
-
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-        ];
-      in
-      rec {
-        defaultPackage = packages.dns-agent;
-        nixosModules = rec {
-          dns-agent = import ./module.nix;
-          default = dns-agent;
-        };
-        packages =
-          {
-            dns-agent = naersk'.buildPackage {
-              src = ./.;
-              nativeBuildInputs = nativeBuildInputs;
-              buildInputs = buildInputs;
-            };
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
           };
 
+          naersk' = pkgs.callPackage naersk { };
 
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs;
-            [
-              nixpkgs-fmt
-              cmake
-              rustc
-              rustfmt
-              cargo
-              clippy
-            ] ++ buildInputs ++ nativeBuildInputs;
-        };
-      }
-    );
+          buildInputs = with pkgs; [
+            openssl.dev
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+        in
+        rec {
+          checks = {
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                clippy = {
+                  enable = true;
+                  packageOverrides = {
+                    cargo = pkgs.cargo;
+                    clippy = pkgs.clippy;
+                  };
+                };
+                rustfmt = {
+                  enable = true;
+                  package = pkgs.rustfmt;
+                };
+                nixpkgs-fmt.enable = true;
+              };
+            };
+          };
+          defaultPackage = packages.dns-agent;
+          nixosModules = rec {
+            dns-agent = import ./module.nix;
+            default = dns-agent;
+          };
+          packages =
+            {
+              dns-agent = naersk'.buildPackage {
+                src = ./.;
+                nativeBuildInputs = nativeBuildInputs;
+                buildInputs = buildInputs;
+              };
+            };
+
+
+          devShells.default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            nativeBuildInputs = with pkgs;
+              [
+                nixpkgs-fmt
+                cmake
+                rustc
+                rustfmt
+                cargo
+                clippy
+              ] ++ buildInputs ++ nativeBuildInputs;
+          };
+        }
+      );
 }
